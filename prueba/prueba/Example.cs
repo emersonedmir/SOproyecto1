db1d30795e1cb3920075297d426fd94221f9ecc2
EmersonMiranda <emersonedmir@gmail.com>
2018-09-07 08:51:08 -0600

    prueba funcional de los hilos

---
+++ prueba/prueba/Example.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+   
+
+    public class Example
+    {
+        // Create a new Mutex. The creating thread does not own the mutex.
+        private static Mutex mut = new Mutex();
+        private const int numIterations = 2;
+        private const int numThreads = 5;
+        public static List<int> listanum = new List<int>();
+
+        public void listar(int numero)
+        {
+            listanum.Add(numero);
+        }
+        public int nElemntos()
+        {
+            return listanum.Count;
+        }
+        public void Main()
+        {
+            for (int nm = 0; nm < 2; nm++)
+            {
+                listanum.Add(nm);
+            }
+            // Create the threads that will use the protected resource.
+            for (int i = 0; i < numThreads; i++)
+            {
+                Thread newThread = new Thread(new ThreadStart(ThreadProc));
+                newThread.Name = String.Format("Thread{0}", i + 1);
+                newThread.Start();
+            }
+
+            Console.Read();
+        }
+
+        private static void ThreadProc()
+        {
+            while (true)
+            {
+                UseResource();
+            }
+        }
+
+        private static void UseResource()
+        {
+
+            mut.WaitOne();//espera turno para entrar al recurso compartido
+
+
+
+            
+                Console.WriteLine("proceso: {0}    lista[{1}] ", Thread.CurrentThread.Name,
+                              listanum[0].ToString());
+          
+
+
+
+            // libera el recurso compartido para que entre otro
+            mut.ReleaseMutex();
+
+
+        }
+    }
+
+}
---
+++ prueba/prueba/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int n = 0;
+        public Example hilos; 
+        public Form1()
+        {
+            hilos = new Example();
+            hilos.Main();
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            n = hilos.nElemntos();
+            hilos.listar(1);
+            listBox1.Items.Add(n.ToString());
+
+        }
+    }
+}
